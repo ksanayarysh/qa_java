246ad4ef3e7e7d4da6e3b9a45a4f64449902df86
Ksenia Yarysh <ksana32@gmail.com>
2019-06-09 15:27:43 +0000

    gr

--- src/test/java/lesson1/ChromeTest.java
+++ src/test/java/lesson1/ChromeTest.java
@@ -1,15 +1,15 @@
 package lesson1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lesson1.grafana.ExecutionListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
+
+@Listeners({ExecutionListener.class})
 public class ChromeTest {
 
     private WebDriver driver;
@@ -20,26 +20,25 @@ public class ChromeTest {
         WebDriverManager.chromedriver().setup();
     }
 
-    @Before
+    @BeforeTest
     public void setupTest() {
         driver = new ChromeDriver();
     }
 
-    @After
+    @AfterTest
     public void teardown() {
         if (driver != null) {
             driver.quit();
         }
     }
 
-    @Test
+    @Test(description = "chrome test", groups = {"web tests"})
     public void test() {
-        logger.info("dddd");
-        logger.info("dddd");
-        logger.info("dddd");
+        logger.info("very very beginning");
+        logger.info("start");
         driver.get("http://otus.ru");
-        logger.info("dddd");
-        logger.info("dddd");
+        logger.info("stop");
+        logger.info("very stop");
     }
 
 }
---
+++ src/test/java/lesson1/grafana/GrafanaTests.java
@@ -0,0 +1,68 @@
+package lesson1.grafana;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
+
+@Listeners({ExecutionListener.class})
+public class GrafanaTests {
+
+    public void doSleep() throws InterruptedException {
+        sleep((int) (Math.random() * 20000));
+    }
+
+    @Test(description = "test1", groups = {"group 1"})
+    public void test1() throws InterruptedException {
+        assert 1 == 1;
+        doSleep();
+    }
+
+    @Test(description = "test2", groups = {"group 1"})
+    public void test2() throws InterruptedException {
+        assert 1 == 5;
+        doSleep();
+    }
+
+    @Test(description = "test3", groups = {"group 1"})
+    public void test3() throws InterruptedException {
+        assert 1 == 5;
+        doSleep();
+    }
+
+    @Test(description = "test4", groups = {"group 2"})
+    public void test4() throws InterruptedException {
+        assert 1 == 1;
+        doSleep();
+    }
+
+    @Test(description = "test5", groups = {"group 2"})
+    public void test5() throws InterruptedException {
+        assert 1 == 1;
+        doSleep();
+    }
+
+    @Test(description = "test6", groups = {"group 2"})
+    public void test6() throws InterruptedException {
+        assert 1 == 1;
+        doSleep();
+    }
+
+    @Test(description = "test7", groups = {"group 2"})
+    public void test7() throws InterruptedException {
+        assert 1 == 1;
+        doSleep();
+    }
+
+    @Test(description = "test8", groups = {"group 2"})
+    public void test8() throws InterruptedException {
+        assert 1 == 1;
+        doSleep();
+    }
+
+    @Test(description = "test9", groups = {"group 2"})
+    public void test9() throws InterruptedException {
+        assert 1 == 10;
+        doSleep();
+    }
+}
